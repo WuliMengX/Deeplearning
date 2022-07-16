a77fb3da84a7a934553c726864c1a58d4e1712e3
WuliMengX <wulimengx@Outlook.com>
2022-07-17 07:43:06 +0800

    VScode

--- Python Learning/4-pandas/10-TimeData.py
+++ Python Learning/4-pandas/10-TimeData.py
@@ -14,7 +14,19 @@
     df['Date'].dt.day_name()    返回一个Series，一一对应原数据是星期几
                                 Date列的数据为转换过的标准时间类型，day_name指星期几
 
-    
-    
-    
+    df['Date'].min()    最远日期
+    df['Date'].max()    最近日期
+        两者可以相减运算
+           
+    filt = (df['Date'] >= '2020')   pandas知道这个2020的含义是年  
+                                    如果要更精确可以换成 pd.to_datetime('年-月-日') 
+    df.loc[filt]                    筛选出年份大于等于2020的DataFrame
+
+    df.set_index('Date',inplace=True)   甚至可以以时间为index
+    df['2019']                          这样筛选可以变得更简单
+    df['2020-01':'2020-02']             可以如此使用切片
+    df[lable].resample('D').max()       标签为lable的列重新采样，index以天为单位('D')，每一天的最大值
+    df.resample('W').mean()             index以周为单位，columns在一周内的平均值
+    df.resample('W').agg({lable:'函数名'...})   各个lable的数据各用一个函数处理
+      
 """
---
+++ Python Learning/4-pandas/11-ReadWrite.py
@@ -0,0 +1,14 @@
+"""Reading/Writing Data to Different Sources - Excel, JSON, SQL, Etc"""
+""" 
+    excel安装包：
+    pip install xlwt openpyxl xlrd
+
+    df = pd.read_excel(path,)
+    df.to_excel(path)
+    
+    连接SQL DATABASE：
+        pip install SLAlchemy
+
+        from sqlalchemy import create_engine
+        import 相应database名
+"""
