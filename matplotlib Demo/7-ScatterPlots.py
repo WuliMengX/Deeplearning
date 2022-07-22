d9edab5915845f782ac543342a9b0aabf448e159
WuliMengX <wulimengx@outlook.com>
2022-07-22 09:37:11 +0800

    Pycharm

---
+++ matplotlib Demo/7-ScatterPlots.py
@@ -0,0 +1,26 @@
+
+import pandas as pd
+from matplotlib import pyplot as plt
+
+plt.style.use('seaborn')
+
+data = pd.read_csv('sources/2019-05-31-data.csv')
+view_count = data['view_count']
+likes = data['likes']
+ratio = data['ratio']
+
+plt.scatter(view_count, likes, c=ratio, cmap='summer',edgecolors='black', linewidth=1, alpha=0.75)
+
+cbar = plt.colorbar()
+cbar.set_label('Ratio')
+
+plt.xscale('log')
+plt.yscale('log')
+
+plt.title('Trending YouTube Videos')
+plt.xlabel('View Count')
+plt.ylabel('Total Likes')
+
+plt.tight_layout()
+
+plt.show()
