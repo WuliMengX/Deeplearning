b10b5293925ae143bafdaa7f30f7b53a8e866ae8
WuliMengX <wulimengx@outlook.com>
2022-07-25 23:31:13 +0800

    ubuntu

--- Python Learning/6-machinelearning/J老师的课/2-线性回归/LinearRegression.py
+++ Python Learning/6-machinelearning/J老师的课/2-线性回归/LinearRegression.py
@@ -23,5 +23,26 @@
         MAE平均绝对误差     推荐
         R-Squared准确率    from sklearn.metrics import r2_score
                            r2_score(X_test,y_test)
-                               
+    
+    大多数情况下，应该避免使用纯线性回归，岭回归是个不错的默认选择
+    如果实际用到的特征只有少数几个，倾向于使用Lasso回归或者弹性网络，因为它们会将无用的特征权重降为0
+    一般弹性网络优于Lasso回归。特征数量超过样本数或几个特征强相关的时候Lasso回归表现不稳定
+    
 """
+"""参考代码"""
+from sklearn.datasets import load_boston
+from sklearn.linear_model import LassoCV, ElasticNetCV
+
+boston = load_boston()
+
+lscv = LassoCV(alphas=[1.0, 0.1, 0.01, 0.001, 0.005, 0.0025, 0.001, 0.00025], normalize=True)
+
+lscv.fit(boston.data, boston.target)
+
+print('Lasso optimal alpha:%.3f' % lscv.alpha_)
+
+encv = ElasticNetCV(alphas=[1.0, 0.01, 0.005, 0.0025, 0.001], l1_ratio=(0.1, 0.25, 0.5, 0.75, 0.9), normalize=True)
+
+encv.fit(boston.data, boston.target)
+
+print('ElasticNet optimal alpha:%.3f and L1 ratio:%.4f' % (encv.alpha_, encv.l1_ratio_))
